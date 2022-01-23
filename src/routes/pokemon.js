0589783c2add1431d3dcdd0fad81c0be19e13820
diemora473 <diego.e.morales01@gmail.com>
2022-01-23 00:05:37 -0300

    make it better

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -32,32 +32,32 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-//GET POKEMON BY ID PASSED BY PARAMS (:)
-// router.get('/:id', async (req, res, next) => {
-//     try {
-//         const { id } = req.params //el id viene cuando voy al detalle del pokemon, el id se agrega a la ruta 
-//         const getAllPokemon = await getAllPk()
-//         const result = getAllPokemon.find(e => e.id == id) // find, me devuelve el primer elemento del id que yo estoy buscando, findIndex no, porque me devuelve un numero, y yo quiero el elemento
-//         return res.json(result)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
+// GET POKEMON BY ID PASSED BY PARAMS (:)
 router.get('/:id', async (req, res, next) => {
     try {
-        const { id } = req.params;
-        const allId = id.includes("-")
-        if (allId) {
-            const pokemon = await Pokemon.findByPk(id)
-            res.send(pokemon)
-        } else {
-            const pokemonApi = await axios.get('https://pokeapi.co/api/v2/pokemon/' + id)
-            res.send(pokemonApi)
-        }
+        const { id } = req.params //el id viene cuando voy al detalle del pokemon, el id se agrega a la ruta 
+        const getAllPokemon = await getAllPk()
+        const result = getAllPokemon.find(e => e.id == id) // find, me devuelve el primer elemento del id que yo estoy buscando, findIndex no, porque me devuelve un numero, y yo quiero el elemento
+        return res.json(result)
     } catch (err) {
         next(err)
     }
 })
+// router.get('/:id', async (req, res, next) => {
+//     try {
+//         const { id } = req.params;
+//         const allId = id.includes("-")
+//         if (allId) {
+//             const pokemon = await Pokemon.findByPk(id)
+//             res.send(pokemon)
+//         } else {
+//             const pokemonApi = await axios.get('https://pokeapi.co/api/v2/pokemon/' + id)
+//             res.send(pokemonApi)
+//         }
+//     } catch (err) {
+//         next(err)
+//     }
+// })
 //POST CREATED POKEMONS BY BODY
 router.post('/', async (req, res, next) => {
     try {
