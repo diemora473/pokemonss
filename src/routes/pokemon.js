c7f5e0358574e4656cbc7579abdfcae4a03d2843
diemora473 <diego.e.morales01@gmail.com>
2022-01-28 15:21:32 -0300

    probandooo

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -6,8 +6,6 @@ const getPokemonsApi = require('../controllers/getPokemonApi')
 const getPokemonDb = require('../controllers/getPokemonDb');
 const getAllPk = require('../controllers/getPkAll')
 const createPokemon = require('../controllers/createPokemon')
-const app = express()
-const cors = require('cors')
 
 
 
