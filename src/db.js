b61ed5cb7421f3de6fd5dd5a8b96e81b74706ce1
diemora473 <diego.e.morales01@gmail.com>
2022-01-29 01:54:25 -0300

    agregandoFunciones

--- src/db.js
+++ src/db.js
@@ -16,6 +16,14 @@ const sequelize = new Sequelize(DATABASE_URL, {
     }
   }
 });
+// const {
+//   DB_USER, DB_PASSWORD, DB_HOST,
+// } = process.env;
+
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
